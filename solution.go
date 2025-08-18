5b51a492f1b620dbae0c4c2424332915c2376bc0
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-08-19 02:43:47 +0530

    19 august

--- solution.go
+++ solution.go
@@ -4,51 +4,50 @@ import (
 	"fmt"
 )
 
-// [OLD] https://takeuforward.org/data-structure/count-occurrences-in-sorted-array/
-// [OLD] https://takeuforward.org/data-structure/reverse-words-in-a-string/
-// [OLD] https://takeuforward.org/data-structure/find-intersection-of-two-linked-lists/
+// [OLD] https://leetcode.com/problems/power-of-two/description/
+// [OLD] https://takeuforward.org/data-structure/rotate-image-by-90-degree/
+// [OLD] https://takeuforward.org/data-structure/merge-overlapping-sub-intervals/
 // [NEW] https://takeuforward.org/arrays/painters-partition-problem/
-// [NEW] https://leetcode.com/problems/roman-to-integer/description/
-// [NEW] https://takeuforward.org/plus/dsa/problems/check-if-there-exists-a-subsequence-with-sum-k
-// [NEW] https://takeuforward.org/data-structure/word-search-leetcode/
-// [NEW] https://leetcode.com/problems/single-number/description/
-// [NEW] https://takeuforward.org/data-structure/implement-min-stack-o2n-and-on-space-complexity/
-// [NEW] https://leetcode.com/problems/maximal-rectangle/description/
-// [NEW] https://leetcode.com/problems/binary-subarrays-with-sum/description/
+// [NEW] https://takeuforward.org/data-structure/check-if-two-strings-are-anagrams-of-each-other/
+// [NEW] https://takeuforward.org/data-structure/power-set-print-all-the-possible-subsequences-of-the-string/
+// [NEW] https://takeuforward.org/data-structure/n-queen-problem-return-all-distinct-solutions-to-the-n-queens-puzzle/
+// [NEW] https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/
+// [NEW] postfix-to-infix: ab-de+f*/
+// [NEW] https://takeuforward.org/data-structure/sliding-window-maximum/
+// [NEW] https://takeuforward.org/data-structure/length-of-longest-substring-without-any-repeating-character/
 
 func main() {
-	nums := []int {1, 0, 1, 0, 1}
-	goal := 2
-	ans := binary(nums, goal)
+	s := "abcabcbb"
+	ans := long(s)
 	fmt.Printf("ans: %d\n", ans)
 
-	nums = []int {0, 0, 0, 0, 0}
-	goal = 0
-	ans = binary(nums, goal)
+	s = "bbbbb"
+	ans = long(s)
+
 	fmt.Printf("ans: %d\n", ans)
 }
 
-func binary(arr []int, goal int) int {
-	return sum(arr, goal)-sum(arr, goal-1)
-}
-
-
-func sum(arr []int, goal int) int {
-	if goal < 0 {
-		return 0
-	}
-	cnt := 0
-	sum := 0
+func long(s string) int {
+	maxLen := 0
 	i, j := 0, 0
 
-	for j < len(arr) {
-		sum += arr[j]%2
-		for sum > goal {
-			sum -= arr[i]
-			i++
+	charMap := map[byte]int {}
+	
+	for j < len(s) {
+
+		if index, ok := charMap[s[j]]; ok {
+			if index+1 > i {
+				i = index+1
+			}
 		}
-		cnt += j-i+1
+
+
+		l := j-i+1
+		if l > maxLen {
+			maxLen = l
+		}
+		charMap[s[j]] = j
 		j++
 	}
-	return cnt
+	return maxLen
 }
