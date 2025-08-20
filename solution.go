00d3072c98b9ca8c87f28af4d72cc77ece0f5f95
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-08-20 11:54:43 +0530

    20th aug

--- solution.go
+++ solution.go
@@ -4,50 +4,80 @@ import (
 	"fmt"
 )
 
-// [OLD] https://leetcode.com/problems/power-of-two/description/
-// [OLD] https://takeuforward.org/data-structure/rotate-image-by-90-degree/
-// [OLD] https://takeuforward.org/data-structure/merge-overlapping-sub-intervals/
-// [NEW] https://takeuforward.org/arrays/painters-partition-problem/
-// [NEW] https://takeuforward.org/data-structure/check-if-two-strings-are-anagrams-of-each-other/
+// [OLD] https://takeuforward.org/data-structure/median-of-row-wise-sorted-matrix/
+// [OLD] https://takeuforward.org/data-structure/peak-element-in-array/
+// [OLD] https://takeuforward.org/data-structure/count-reverse-pairs/
+// [NEW] https://takeuforward.org/arrays/kth-missing-positive-number/
+// [NEW] https://leetcode.com/problems/string-to-integer-atoi/description/
 // [NEW] https://takeuforward.org/data-structure/power-set-print-all-the-possible-subsequences-of-the-string/
-// [NEW] https://takeuforward.org/data-structure/n-queen-problem-return-all-distinct-solutions-to-the-n-queens-puzzle/
-// [NEW] https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/
-// [NEW] postfix-to-infix: ab-de+f*/
-// [NEW] https://takeuforward.org/data-structure/sliding-window-maximum/
-// [NEW] https://takeuforward.org/data-structure/length-of-longest-substring-without-any-repeating-character/
+// [NEW] https://takeuforward.org/data-structure/word-search-leetcode/
+// [NEW] https://leetcode.com/problems/subsets/description/
+// [NEW] https://takeuforward.org/data-structure/check-for-balanced-parentheses/
+// [NEW] https://leetcode.com/problems/remove-k-digits/
+// [NEW] https://leetcode.com/problems/longest-repeating-character-replacement/description/
 
 func main() {
-	s := "abcabcbb"
-	ans := long(s)
+	arr := []int {1,3,2,3,1}
+	ans := count(arr)
 	fmt.Printf("ans: %d\n", ans)
 
-	s = "bbbbb"
-	ans = long(s)
-
+	arr = []int {3,2,1,4}
+	ans = count(arr)
 	fmt.Printf("ans: %d\n", ans)
 }
 
-func long(s string) int {
-	maxLen := 0
-	i, j := 0, 0
+func count(arr []int) int {
+	return mergeSort(arr, 0, len(arr)-1)
+}
 
-	charMap := map[byte]int {}
-	
-	for j < len(s) {
-
-		if index, ok := charMap[s[j]]; ok {
-			if index+1 > i {
-				i = index+1
-			}
-		}
-
-
-		l := j-i+1
-		if l > maxLen {
-			maxLen = l
-		}
-		charMap[s[j]] = j
-		j++
+func mergeSort(arr []int, low, high int) int {
+	cnt := 0
+	if low < high {
+		mid := (low+high)/2
+		cnt += mergeSort(arr, low, mid)
+		cnt += mergeSort(arr, mid+1, high)
+		cnt += countPairs(arr, low, mid, high)
+		merge(arr, low, mid, high)
 	}
-	return maxLen
+	return cnt
+}
+
+func countPairs(arr []int, low, mid, high int) int {
+	right := mid + 1
+	cmt := 0
+	for i := low; i <= mid; i++ {
+		for right <= high && arr[i] > 2*arr[right] {
+			right++
+		}
+		cmt += (right-(mid+1))
+	}
+	return cmt
+}
+
+func merge(arr []int, low, mid, high int) {
+	left, right := low, mid + 1
+	tmp := []int {}
+
+	for left <= mid && right <= high {
+		if arr[left] <= arr[right] {
+			tmp = append(tmp, arr[left])
+			left++
+		}else {
+			tmp = append(tmp, arr[right])
+			right++
+		}
+	}
+
+	for left <= mid + 1 {
+		tmp = append(tmp, arr[left])
+		left++
+	}
+	for right <= high {
+		tmp = append(tmp, arr[right])
+		right++
+	}
+
+	for i := low; i <= high; i++ {
+		arr[i] = tmp[i-low]
+	}
 }
