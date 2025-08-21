ea720a0525db45e6a65875a45c2e16d4494c0deb
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-08-21 06:19:39 +0530

    21 aug

--- solution.go
+++ solution.go
@@ -17,67 +17,35 @@ import (
 // [NEW] https://leetcode.com/problems/longest-repeating-character-replacement/description/
 
 func main() {
-	arr := []int {1,3,2,3,1}
-	ans := count(arr)
-	fmt.Printf("ans: %d\n", ans)
-
-	arr = []int {3,2,1,4}
-	ans = count(arr)
+	s := "AABABBBCCD"
+	k := 2
+	ans := longest(s, k)
 	fmt.Printf("ans: %d\n", ans)
 }
 
-func count(arr []int) int {
-	return mergeSort(arr, 0, len(arr)-1)
-}
+func longest(s string, k int) int {
+	maxlen := 0
+	hashArr := [26]int {}
+	maxF := 0
 
-func mergeSort(arr []int, low, high int) int {
-	cnt := 0
-	if low < high {
-		mid := (low+high)/2
-		cnt += mergeSort(arr, low, mid)
-		cnt += mergeSort(arr, mid+1, high)
-		cnt += countPairs(arr, low, mid, high)
-		merge(arr, low, mid, high)
-	}
-	return cnt
-}
-
-func countPairs(arr []int, low, mid, high int) int {
-	right := mid + 1
-	cmt := 0
-	for i := low; i <= mid; i++ {
-		for right <= high && arr[i] > 2*arr[right] {
-			right++
+	i, j := 0, 0
+	for j < len(s) {
+		hashArr[s[j]-'A']++
+		if maxF < hashArr[s[j]-'A'] {
+			maxF = hashArr[s[j]-'A']
 		}
-		cmt += (right-(mid+1))
-	}
-	return cmt
-}
 
-func merge(arr []int, low, mid, high int) {
-	left, right := low, mid + 1
-	tmp := []int {}
-
-	for left <= mid && right <= high {
-		if arr[left] <= arr[right] {
-			tmp = append(tmp, arr[left])
-			left++
-		}else {
-			tmp = append(tmp, arr[right])
-			right++
+		if (j-i+1) - maxF > k {
+			hashArr[s[i]-'A']--
+			i++
 		}
-	}
 
-	for left <= mid + 1 {
-		tmp = append(tmp, arr[left])
-		left++
+		if (j-i+1)-maxF <= k {
+			if maxlen < j-i+1 {
+				maxlen = j-i+1
+			}
+		}
+		j++
 	}
-	for right <= high {
-		tmp = append(tmp, arr[right])
-		right++
-	}
-
-	for i := low; i <= high; i++ {
-		arr[i] = tmp[i-low]
-	}
+	return maxlen
 }
