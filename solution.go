b579096d874ced2d3c4619a54c9b2795e6a61c03
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-08-23 00:02:34 +0530

    22 aug

--- solution.go
+++ solution.go
@@ -4,47 +4,56 @@ import (
 	"fmt"
 )
 
-// [OLD] https://takeuforward.org/data-structure/median-of-row-wise-sorted-matrix/
-// [OLD] https://takeuforward.org/data-structure/peak-element-in-array/
-// [OLD] https://takeuforward.org/data-structure/count-reverse-pairs/
-// [NEW] https://takeuforward.org/arrays/kth-missing-positive-number/
-// [NEW] https://leetcode.com/problems/string-to-integer-atoi/description/
-// [NEW] https://takeuforward.org/data-structure/power-set-print-all-the-possible-subsequences-of-the-string/
-// [NEW] https://takeuforward.org/data-structure/word-search-leetcode/
-// [NEW] https://leetcode.com/problems/subsets/description/
+// [OLD] https://takeuforward.org/data-structure/find-intersection-of-two-linked-lists/
+// [OLD] https://takeuforward.org/data-structure/merge-overlapping-sub-intervals/
+// [OLD] https://takeuforward.org/binary-search/find-peak-element-ii
+// [NEW] https://takeuforward.org/data-structure/aggressive-cows-detailed-solution/
+// [NEW] https://leetcode.com/problems/roman-to-integer/description/
+// [NEW] https://takeuforward.org/data-structure/combination-sum-ii-find-all-unique-combinations/
+// [NEW] https://takeuforward.org/data-structure/palindrome-partitioning/
+// [NEW] https://takeuforward.org/plus/dsa/problems/xor-of-numbers-in-a-given-range
 // [NEW] https://takeuforward.org/data-structure/check-for-balanced-parentheses/
-// [NEW] https://leetcode.com/problems/remove-k-digits/
+// [NEW] https://leetcode.com/problems/online-stock-span/description/
 // [NEW] https://leetcode.com/problems/longest-repeating-character-replacement/description/
 
 func main() {
-	s := "AABABBBCCD"
-	k := 2
-	ans := longest(s, k)
+	s := "ABAA"
+	k := 0
+	ans := characterReplacement(s, k)
+	fmt.Printf("ans: %d\n", ans)
+
+	s = "AABABBA"
+	k = 1
+	ans = characterReplacement(s, k)
 	fmt.Printf("ans: %d\n", ans)
 }
 
-func longest(s string, k int) int {
+func characterReplacement(s string, k int) int {
 	maxlen := 0
-	hashArr := [26]int {}
-	maxF := 0
-
 	i, j := 0, 0
+	maxf := 0
+	charmap := [26]int {}
+
 	for j < len(s) {
-		hashArr[s[j]-'A']++
-		if maxF < hashArr[s[j]-'A'] {
-			maxF = hashArr[s[j]-'A']
+		charmap[s[j]-'A']++
+
+		if maxf < charmap[s[j]-'A'] {
+			maxf = charmap[s[j]-'A']
 		}
 
-		if (j-i+1) - maxF > k {
-			hashArr[s[i]-'A']--
+		n := j-i+1-maxf
+
+		if n > k {
+			charmap[s[i]-'A']--
 			i++
 		}
 
-		if (j-i+1)-maxF <= k {
-			if maxlen < j-i+1 {
+		if j-i+1-maxf <= k {
+			if j-i+1 > maxlen {
 				maxlen = j-i+1
 			}
 		}
+		
 		j++
 	}
 	return maxlen
